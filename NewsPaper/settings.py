3c8d96ca717b07c9ab2ac9b9e70531db0f03d7d7
ArtiomIV <ivan199821zxc@gmail.com>
2021-05-31 22:10:09 +0200

    Celery/cache added

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -203,7 +203,7 @@ CAHES = {
     'default':{
         'TIMEOUT': 60,
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': os.path.join(BASE_DIR, '/cache_files'), # Указываем, куда будем сохранять кэшируемые файлы! Не забываем создать папку cache_files внутри папки с manage.py!
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'), # Указываем, куда будем сохранять кэшируемые файлы! Не забываем создать папку cache_files внутри папки с manage.py!
     }
 }
 
